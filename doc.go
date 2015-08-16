85fdbaf699ecec4506729696ff6df97398810ca2
nicolas riesch <nicolas.riesch@gmail.com>
2015-08-16 02:27:08 +0200

    fixed doc.go

--- doc.go
+++ doc.go
@@ -8,14 +8,14 @@ Example of use:
 
 	var (
 		ctx decnum.Context
-		a   decnum.DecQuad  // unlike Go variable, the uninitialized value is not zero, but garbage.
-		b   decnum.DecQuad  //    so, if you need a variable to be 0, do         x = decnum.Zero()
-		r   decnum.DecQuad
+		a   decnum.Quad  // unlike Go variable, the uninitialized value is not zero, but garbage.
+		b   decnum.Quad  //    so, if you need a variable to be 0, do         x = decnum.Zero()
+		r   decnum.Quad
 	)
 
-	ctx.Init(decnum.DEFAULT_DECQUAD) // initialize context with default settings for DecQuad operations. Essentially, it contains the rounding mode.
+	ctx.Init(decnum.DEFAULT_DECQUAD) // initialize context with default settings for Quad operations. Essentially, it contains the rounding mode.
 
-	a = ctx.FromString("1234.5678") // convert string to DecQuad. If error, a status bit in ctx will be set.
+	a = ctx.FromString("1234.5678") // convert string to Quad. If error, a status bit in ctx will be set.
 	b = ctx.FromString("-45.7")     //   Error bits in status can be tested with ctx.Error() at any time. Errors are cumulative, and only ctx.ResetStatus() will clear them.
 
 	r = ctx.Add(a, b) // r = a + b       If ctx already contains an error in status, the result of any arithmetic operation is undefined, most probably Nan.
@@ -35,7 +35,7 @@ Tech note
 This package uses cgo to call functions in the C decNumber package.
 
     All parameters are sent and received BY VALUE, because they are small.
-    decQuad is only 128 bits, Context are also sent and received by value because they are small struct (28 bytes). Sane for strings, which are small arrays embedded in struct.
+    Quad is only 128 bits, Context are also sent and received by value because they are small struct (28 bytes). Sane for strings, which are small arrays embedded in struct.
     This way, there is no need to make complex things with pointers between Go and C world, and it is as fast, or even faster.
 
 */
